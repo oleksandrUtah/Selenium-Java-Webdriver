85a67bd6523e2cfba0857195c92e3f37b9965df0
AlexUtah <ste.kozachenko@outlook.com>
2018-12-29 06:12:50 -0700

    12/29/2018

--- src/test/java/day5/CodeEx2.java
+++ src/test/java/day5/CodeEx2.java
@@ -1,15 +1,11 @@
 package day5;
 
-
 import org.testng.annotations.Test;
 
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-
 public class CodeEx2 {
-
-
 
     //input string "welcometojava" // if param is 3 then it's "wel", "elc", "lco"
 
@@ -17,14 +13,7 @@ public class CodeEx2 {
 
     //print out first and last substring from ordered list
 
-
-
-
-
-
-
     @Test
-
     public void test001() {
 
         //receive the input value
@@ -35,19 +24,11 @@ public class CodeEx2 {
 
         //print out fist and last members of list
 
-
-
         SortedSet<String> set = new TreeSet<String>();
-
-
 
         String input = "welcometojava";
 
         int subStringLength = 3;
-
-
-
-
 
         for(int i = 0; i <= input.length() - subStringLength; i++){
 
@@ -55,20 +36,12 @@ public class CodeEx2 {
 
             int endIndex = i + subStringLength;
 
-
-
             String subString = input.substring(beginIndex, endIndex);
 
             set.add(subString);
-
         }
-
-
-
         System.out.println(set.first());
 
         System.out.println(set.last());
-
     }
-
 }
