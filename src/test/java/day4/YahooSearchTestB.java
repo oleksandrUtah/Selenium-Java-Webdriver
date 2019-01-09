17da88a5e8884837358e6e5e0b2947fb9d335666
AlexUtah <ste.kozachenko@outlook.com>
2019-01-09 09:17:15 -0700

    01/09/2019

---
+++ src/test/java/day4/YahooSearchTestB.java
@@ -0,0 +1,40 @@
+package day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class YahooSearchTestB extends BaseTest{
+
+    By searchBox = By.id("uh-search-box");
+
+    @Test
+    public void testSearch() {
+        String queryString = "Portnov School";
+        String url = "https://www.yahoo.com/";
+
+
+        openPage(url);
+        sendKeysTo(searchBox, queryString);
+        submitSearch(searchBox);
+        assertResults();
+    }
+
+    //refactor with usage of BaseTest/PageObjectModel
+    private void assertResults() {
+        By resultsByCSS = By.cssSelector("#yui_3_10_0_1_1543460928491_407");
+        By resultXPath = By.xpath("//*[@id=\"yui_3_10_0_1_1543460928491_407\"]");
+
+        WebElement resultSpanElement = driver.findElement(By.id("results"))
+                .findElement(By.className("compPagination")).findElement(By.tagName("span"));
+
+
+        String xpathValue = "//*[@class='compPagination']/span";
+        String cssValue = "div.compPagination span";
+        By resultsElement = By.xpath(xpathValue);
+
+        explicitWait(driver, By.cssSelector(cssValue));
+        Assert.assertTrue(resultSpanElement.isDisplayed());
+    }
+}
