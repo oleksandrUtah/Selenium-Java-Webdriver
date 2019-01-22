ecd9191bdf0d21edacfe53573e47f88b991f9150
AlexUtah <ste.kozachenko@outlook.com>
2019-01-22 05:47:16 -0700

    01/22/2019

--- src/test/java/day7/ParametersTests.java
+++ src/test/java/day7/ParametersTests.java
@@ -1,43 +1,15 @@
 package day7;
-
-
-
 import org.testng.annotations.Parameters;
-
 import org.testng.annotations.Test;
-
-
-
 public class ParametersTests {
-
-
-
     @Parameters({ "browserName" })
-
     @Test
-
     public void testSingleString(String browserName) {
-
         System.out.println("Invoked testString " + browserName);
-
-
-
     }
-
-
-
     @Parameters({ "browserName" })
-
     @Test
-
     public void testSingleString002(String browserName) {
-
         System.out.println("Invoked testString " + browserName);
-
-
-
     }
-
-
-
 }
