5c51a717b3d244cb7ddd020e9b813f265ceaf3d9
AlexUtah <ste.kozachenko@outlook.com>
2019-03-29 15:52:41 -0600

    03/29/2019

---
+++ src/test/java/CI/HelloWorld.java
@@ -0,0 +1,10 @@
+package CI;
+
+/*
+The HelloWorld class simply prints "Hello World"
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
