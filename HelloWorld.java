e4065e40a53028dc1045951b65b4a346040a2118
AlexUtah <ste.kozachenko@outlook.com>
2019-03-30 06:52:15 -0600

    03/30/2019

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-
-//The HelloWorld class simply prints "Hello World"
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
