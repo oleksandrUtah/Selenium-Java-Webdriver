1b4321edf66f23f5108648594e6f516afa5497a9
AlexUtah <ste.kozachenko@outlook.com>
2019-05-07 12:19:55 -0600

    05/07/2019

---
+++ src/test/java/realAutomation/AllLinks.java
@@ -0,0 +1,57 @@
+package realAutomation;
+import org.openqa.selenium.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+public class AllLinks {
+    String baseUrl = "https://www.google.com/";
+    WebDriver driver;
+    @BeforeTest
+    public void launchBrowser() {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") +
+                "\\src\\test\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+    }
+    @AfterTest
+    public void terminateBrowser(){
+        driver.quit();
+    }
+    @Test(priority = 0)
+    public void testAllLinks() {
+        //create list of all linkelements in the web page:
+        List<WebElement> linkElements = driver.findElements(By.xpath("//a"));
+        //number of all links:
+        int numberLinks = linkElements.size();
+        System.out.println("number of Links: " + numberLinks);
+        //create empty arrays for all linkTexts & linkUrls:
+        String[] linkTexts = new String[numberLinks];
+        String[] linkUrls = new String[numberLinks];
+        //processing linkElements array elements
+        int i = 0;
+        for (WebElement link : linkElements) {
+            linkTexts[i] = link.getText();
+            System.out.println(i + " - link text = " + linkTexts[i]);
+            linkUrls[i] = link.getAttribute("href");
+            System.out.println("    link Attribute href =   " + linkUrls[i]);
+            i++;
+        }
+        //processing linkUrls array elements
+        int m = 0;
+        for (String t : linkUrls) {
+            try {
+                driver.navigate().to(t);
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                System.out.println(m + " - pageTitle = " + driver.getTitle());
+            }
+            catch (RuntimeException e1) {
+                System.err.println("Caught RuntimeException: " + e1.getMessage());
+            }
+            m++;
+        }
+    }
+}
+
